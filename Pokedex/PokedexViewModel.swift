554658e3f415e8c411e71ec97661e36580a518d4
Andressa Silva <neumm.andressa@gmail.com>
2023-01-19 15:19:24 -0300

    Setting up PokedexViewModel

--- Pokedex/PokedexViewModel.swift
+++ Pokedex/PokedexViewModel.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
-struct PokedexViewModel {
+class PokedexViewModel: ObservableObject {
+    @Published var pokemonList: PokemonList = PokemonList(results: [])
     
+    func getPokemonData() {
+        let urlString = "https://pokeapi.co/api/v2/pokemon"
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!) { data, _, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(PokemonList.self, from: data)
+                        self.pokemonList = decodedData
+                    } catch {
+                        print("Something went wrong..." )
+                    }
+                }
+            }
+        }.resume()
+    }
 }
