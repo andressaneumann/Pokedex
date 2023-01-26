2fcff103efa0af5d71edaab64a001c317e078efa
Andressa Silva <neumm.andressa@gmail.com>
2023-01-26 18:15:23 -0300

    Removing unnecessary code

--- Pokedex/Pokemon/Views/PokemonImage.swift
+++ Pokedex/Pokemon/Views/PokemonImage.swift
@@ -20,21 +20,6 @@ struct PokemonImage: View {
             .onAppear {
                 self.getSprite(url: imageLink)
             }
-        
-//        AsyncImage(url: URL(string: pokemonSprite))
-//            .frame(width: 75, height: 75)
-//            .onAppear {
-//                let loadedData = UserDefaults.standard.string(forKey: imageLink)
-//
-//                if loadedData == nil {
-//                    getSprite(url: imageLink)
-//                    UserDefaults.standard.set(imageLink, forKey: imageLink)
-//                } else {
-//                    getSprite(url: loadedData!)
-//                }
-//            }
-//            .clipShape(Circle())
-//            .foregroundColor(Color.gray.opacity(0.6))
     }
     
     func getSprite(url: String) {
