2d6a3ae251cba43bb7049f33f9809e9a6ae6e24a
Andressa Silva <neumm.andressa@gmail.com>
2023-01-27 18:33:10 -0300

    Adding custom colors

---
+++ Pokedex/Pokemon/Custom/CustomColor.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColor.swift
+//  Pokedex
+//
+//  Created by Andressa Neumann Silva on 27/01/23.
+//
+
+import SwiftUI
+
+struct CustomColor {
+    static let customGreen = Color("customGreen")
+    static let customBlue = Color("customBlue")
+    static let customYellow = Color("customYellow")
+    static let customRose = Color("customRose")
+}
